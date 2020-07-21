23afb71a19a7055d8be3d4bf1700b8ace0b08e3f
nonfaketion <junming46@gmail.com>
2020-07-21 15:00:30 -0700

    Initital Setup, established empty functions inside main.js, Added titles, forms, button in index,html. styled title and forms in style.css.

---
+++ main.js
@@ -0,0 +1,28 @@
+let List = [];
+
+function Item(title, info, price, isDone) {
+  this.title = title;
+  this.info = info;
+  this.price = price;
+  this.isDone = isDone;
+
+}
+
+function addItemToList() {
+  const newItem = new Item(title, ingo, price, isDone);
+  List.push(newItem);
+}
+
+function deleteItemFromList(){
+
+}
+
+function editItemFromList(){
+
+}
+function toggleItem(){
+    
+}
+function RenderList(){
+
+}
